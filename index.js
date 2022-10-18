3a050c08ec4238cb4d3d57ff487f9eac4b806fb0
kchn9 <mat.kuchnia@gmail.com>
2022-10-18 14:53:32 +0200

    Add logging middleware

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
 app.use(express.json());
+const morgan = require("morgan");
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      "| body:",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 // Run server
 app.listen(PORT, () => {
-  console.log(`%cServer is running on PORT:${PORT}`);
+  console.log(`Server is running on PORT:${PORT}`);
 });

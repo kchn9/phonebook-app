8b7f7787bb215d11fa52a6853e889e2e172d6efb
kchn9 <mat.kuchnia@gmail.com>
2022-10-19 15:08:03 +0200

    Add static website middleware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require("express");
 const morgan = require("./middleware/morgan");
 const cors = require("cors");
 const app = express();
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 app.use(morgan);
